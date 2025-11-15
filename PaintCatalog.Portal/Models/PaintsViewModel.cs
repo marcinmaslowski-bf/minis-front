90a720fc2c9ba3e2bcade936a2ac4bd0292a047f
marcinmaslowski-bf <marcin.maslowski.alior@gmail.com>
2025-11-15 01:32:19 +0100

    feat: add paints catalog page

---
+++ PaintCatalog.Portal/Models/PaintsViewModel.cs
@@ -0,0 +1,8 @@
+namespace PaintCatalog.Portal.Models
+{
+    public class PaintsViewModel
+    {
+        public string? InitialPaintsJson { get; set; }
+        public string? BrandsJson { get; set; }
+    }
+}
