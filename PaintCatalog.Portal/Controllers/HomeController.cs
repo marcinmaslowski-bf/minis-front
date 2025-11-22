1dc9b976dfe33b84d20ee7866239ae041b6c06eb
marcinmaslowski-bf <marcin.maslowski.alior@gmail.com>
2025-11-22 22:39:00 +0100

    Ensure bearer token is forwarded and allow anonymous home

--- PaintCatalog.Portal/Controllers/HomeController.cs
+++ PaintCatalog.Portal/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PaintCatalog.Portal.ApiClients;
 using PaintCatalog.Portal.Models;
 
 namespace PaintCatalog.Portal.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly IPaintCatalogApiClient _apiClient;
