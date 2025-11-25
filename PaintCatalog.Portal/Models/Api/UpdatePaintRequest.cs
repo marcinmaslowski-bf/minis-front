ff75457acf8298ae54a30fa0b705bb3271443e68
marcinmaslowski-bf <marcin.maslowski.alior@gmail.com>
2025-11-25 22:14:09 +0100

    Update request models for attachment and paint changes

--- PaintCatalog.Portal/Models/Api/UpdatePaintRequest.cs
+++ PaintCatalog.Portal/Models/Api/UpdatePaintRequest.cs
@@ -1,21 +1,41 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace PaintCatalog.Portal.Models.Api
 {
     public class UpdatePaintRequest
     {
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
+
+        [JsonPropertyName("slug")]
         public string? Slug { get; set; }
-        public string? Code { get; set; }
+
+        [JsonPropertyName("type")]
         public PaintType Type { get; set; }
+
+        [JsonPropertyName("finish")]
         public PaintFinish Finish { get; set; }
+
+        [JsonPropertyName("medium")]
         public PaintMedium Medium { get; set; }
+
+        [JsonPropertyName("gradientType")]
+        public GradientType GradientType { get; set; }
+
+        [JsonPropertyName("hexColor")]
         public string? HexColor { get; set; }
+
+        [JsonPropertyName("hexFrom")]
         public string? HexFrom { get; set; }
+
+        [JsonPropertyName("hexTo")]
         public string? HexTo { get; set; }
-        public double? Opacity { get; set; }
-        public double? BottleMl { get; set; }
+
+        [JsonPropertyName("isDiscontinued")]
         public bool IsDiscontinued { get; set; }
+
+        [JsonPropertyName("tagIds")]
         public List<int>? TagIds { get; set; }
     }
 }
