08d61d7bfb1748dfb6ce2c029a08255aed9c34e2
Marcin Maslowski <marcin.maslowski@waseco.pl>
2025-11-28 22:57:30 +0100

    asd

--- PaintCatalog.Portal/Models/Api/PaintMedium.cs
+++ PaintCatalog.Portal/Models/Api/PaintMedium.cs
@@ -2,12 +2,12 @@ namespace PaintCatalog.Portal.Models.Api
 {
     public enum PaintMedium
     {
-        None = 0,
+        Unknown = 0,
         Acrylic = 1,
         Enamel = 2,
         Oil = 3,
         Lacquer = 4,
         Alcohol = 5,
-        Unknown = 99
+        
     }
 }
