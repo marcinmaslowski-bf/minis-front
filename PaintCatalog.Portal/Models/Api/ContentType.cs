14bc7965c4ac34ebc9f6dd4541c057185f476fc6
Marcin Maslowski <marcin.maslowski@waseco.pl>
2025-12-08 20:31:46 +0100

    asd

--- PaintCatalog.Portal/Models/Api/ContentType.cs
+++ PaintCatalog.Portal/Models/Api/ContentType.cs
@@ -5,10 +5,10 @@ namespace PaintCatalog.Portal.Models.Api
     [Flags]
     public enum ContentType
     {
-        Tutorial = 1,
-        BattleReport = 2,
-        Review = 4,
-        Event = 8,
-        News = 16
+        Tutorial = 1 << 0,
+        BattleReport = 1 << 1,
+        Review = 1 << 2,
+        Event = 1 << 3,
+        News = 1 << 4
     }
 }
